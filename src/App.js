a23a13df6abf5c286c4c15dc71a0ae43eb6fd433
reinoptland <reinoptland@gmail.com>
2019-04-05 06:06:15 +0200

    Add state, form and list to App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    quotes: [
+      { id: 1, 
+        content: "Talk is cheap. Show me the code. ", 
+        source: "Linus Torvalds"
+      },
+      { id: 2, 
+        content: "Programs must be written for people to read, and only incidentally for machines to execute. ", 
+        source: "Harold Abelson"
+      },
+      { id: 3, 
+        content: "Give a man a program, frustrate him for a day. Teach a man to program, frustrate him for a lifetime. " , 
+        source: "Muhammad Waseem"
+      },
+    ],
+    content: '',
+    source: ''
+  }
+
+  handleChange = (event) => {
+    const name = event.target.name
+    const value = event.target.value
+    this.setState({ [name]: value })
+  }
+
+  addQuote = (event) => {
+    event.preventDefault()
+    const { source, content, quotes } = this.state
+    this.setState({
+      quotes: quotes.concat({ source, content }),
+      source: "",
+      content: ""
+    })
+  }
+
   render() {
     return (
       <div className="App">
+
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Quotes</h1>
         </header>
+
+        <main>
+
+          <form onSubmit={this.addQuote}>
+            <h2>Add a new quote:</h2>
+            <label htmlFor="content">Content</label>
+            <textarea 
+              name="content" 
+              value={this.state.content}
+              onChange={this.handleChange} 
+            >
+            </textarea>
+            <label htmlFor="source">Source</label>
+            <input 
+              name="source"
+              value={this.state.source}
+              onChange={this.handleChange}/>
+            <input type="submit"/>
+          </form>
+
+          <section>
+            {this.state.quotes.map(quote => {
+              return(
+                <blockquote key={quote.id}>
+                  <p>{quote.content}</p>
+                  <br/>
+                  <b><i>- {quote.source}</i></b>
+                </blockquote>
+                )
+            })}
+          </section>
+
+        </main>
       </div>
     );
   }
